d795d4e57dc895d967d421e5bd65a2d8c9bf26ef
marclmao <marcuslmao1@gmail.com>
2023-10-26 22:38:48 -0400

    oh my fucking god

--- src/hollandFlag.java
+++ src/hollandFlag.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-// 0,0 is the top left
+// 0,0 is the top left l
 // AS Y VALUE GETS BIGGER, GOES DOWN, NEGATIVE OF MATH CLASS
 public class hollandFlag extends JPanel {
     public static void main(String[] args) {
